2ac63cac9e7881ef1ed6060109152d6035bf58e7
Jason Vincent <7390021+vincen8147@users.noreply.github.com>
2018-12-08 13:49:38 -0800

    Startup script

---
+++ src/main/resources/screenshow_start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt
+
+${JAVA_HOME}/bin/java -jar \
+ /opt/screenshow/target/sprinkler-1.0-SNAPSHOT.jar \
+ ${SCREENSHOW_GOOGLE_EMAIL} \
+ ${SCREENSHOW_GOOGLE_APP_ID} &
+
